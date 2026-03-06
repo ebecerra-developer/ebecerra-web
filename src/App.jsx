54008d81558dae1ed444baba96a10b45be2cec50
Enrique Becerra <elquique.bg@gmail.com>
2026-03-06 21:17:45 +0100

    añadir metricas

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Skills from "./components/Skills";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import "./App.css";
 
 export default function App() {
@@ -27,6 +28,7 @@ export default function App() {
       <Contact />
       <Footer />
       <Analytics />
+      <SpeedInsights />
     </div>
   );
 }
